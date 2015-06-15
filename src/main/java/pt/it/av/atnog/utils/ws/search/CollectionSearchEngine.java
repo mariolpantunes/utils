e6efc7d53e29aeb0658b41a12782f2bb4d9efb18
Mario Antunes <mariolpantunes@gmail.com>
2015-06-16 00:48:15 +0100

    Added CollectionSearchEngine.
    Holds a list of search engines, and propagates the operations for them all.

---
+++ src/main/java/pt/it/av/atnog/utils/ws/search/CollectionSearchEngine.java
@@ -0,0 +1,31 @@
+package pt.it.av.atnog.utils.ws.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mantunes on 6/16/15.
+ */
+public class CollectionSearchEngine implements SearchEngine {
+    private final List<SearchEngine> se;
+
+    public CollectionSearchEngine(final List<SearchEngine> se) {
+        this.se = se;
+    }
+
+    @Override
+    public List<String> search(String q) {
+        List<String> rv = new ArrayList<>();
+        for (SearchEngine s : se)
+            rv.addAll(s.search(q));
+        return rv;
+    }
+
+    @Override
+    public List<String> snippets(String q) {
+        List<String> rv = new ArrayList<>();
+        for (SearchEngine s : se)
+            rv.addAll(s.snippets(q));
+        return rv;
+    }
+}
