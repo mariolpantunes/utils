5c97a0eccee212060327bac8e4376326e5a292db
Mario Antunes <mariolpantunes@gmail.com>
2015-07-13 19:50:35 +0100

    Added HTTP post method.
    This methods needs to be reviewed and optimized.

--- src/main/java/pt/it/av/atnog/utils/HTTP.java
+++ src/main/java/pt/it/av/atnog/utils/HTTP.java
@@ -2,10 +2,7 @@ package pt.it.av.atnog.utils;
 
 import pt.it.av.atnog.utils.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Base64;
@@ -14,6 +11,8 @@ import java.util.zip.Inflater;
 import java.util.zip.InflaterInputStream;
 
 //TODO: review the exception for error codes different from OK
+//TODO: test try-resource to minize the amount of code
+//TODO: review put method
 public class HTTP {
     private static final int TIMEOUT = 3000;
 
@@ -157,5 +156,38 @@ public class HTTP {
                 con.disconnect();
         }
         return rv;
+    }
+
+    //TODO: this method needs some love, like all the persons in the world...
+    public static void post(String url, JSONObject json) throws IOException {
+        post(url, json, TIMEOUT);
+    }
+
+    public static void post(String url, JSONObject json, int timeout) throws IOException {
+        byte[] data = json.toString().getBytes("UTF-8");
+        HttpURLConnection con = null;
+        try {
+            con = (HttpURLConnection) new URL(url).openConnection();
+            con.setReadTimeout(timeout);
+            con.setReadTimeout(timeout);
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setRequestProperty("charset", "utf-8");
+            con.setRequestProperty("Accept-Encoding", "gzip, deflate");
+            con.setRequestProperty("Content-Length", Integer.toString(data.length));
+            con.setRequestProperty("User-Agent", "");
+            con.setDoOutput(true);
+            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+                wr.write(data);
+                wr.flush();
+            }
+            if (con.getResponseCode() == HttpURLConnection.HTTP_OK)
+                JSONObject.read(new BufferedReader(new InputStreamReader(inputStream(con))));
+            else
+                throw new IOException("Error code: " + con.getResponseCode());
+        } finally {
+            if (con != null)
+                con.disconnect();
+        }
     }
 }
