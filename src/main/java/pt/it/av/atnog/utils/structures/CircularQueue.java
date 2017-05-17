5c58f804e63b4ffae8fe48cf42264860e342b994
Mário Antunes <mariolpantunes@gmail.com>
2017-05-17 18:39:26 +0100

    Added more unit tests for CircularQueue.

--- src/main/java/pt/it/av/atnog/utils/structures/CircularQueue.java
+++ src/main/java/pt/it/av/atnog/utils/structures/CircularQueue.java
@@ -10,7 +10,6 @@ import java.util.Queue;
 /**
  * Queue implemented with a circular buffer.
  * <p>
- * <p>
  * This queue has a fixed size.
  * After filling the buffer, any subsequent add operation will overweight the oldest element.<br>
  * Currently this structure is used in two differente scenarios:
@@ -25,268 +24,268 @@ import java.util.Queue;
  * @version 1.0
  */
 public class CircularQueue<E> implements Queue<E> {
-    private int head = 0, size = 0;
-    private E data[];
+  private int head = 0, size = 0;
+  private E data[];
 
-    public CircularQueue() {
-        this(10);
+  public CircularQueue() {
+    this(10);
+  }
+
+  public CircularQueue(int size) {
+    this.data = Utils.cast(new Object[size]);
+  }
+
+  @Override
+  public boolean add(E e) {
+    if (e == null)
+      throw new NullPointerException();
+
+    int tail = (head + size) % data.length;
+    data[tail] = e;
+    if (size == data.length)
+      head = (head + 1) % data.length;
+    else
+      size++;
+
+    return true;
+  }
+
+  @Override
+  public boolean remove(Object o) {
+    boolean rv = false;
+    if (contains(o)) {
+      E tdata[] = Utils.cast(new Object[this.data.length]);
+      int tsize = 0;
+      for (E e : this)
+        if (!e.equals(o))
+          tdata[tsize++] = e;
+      this.data = null;
+      this.data = tdata;
+      this.size = tsize;
+      this.head = 0;
+      rv = true;
     }
+    return rv;
+  }
 
-    public CircularQueue(int size) {
-        this.data = Utils.cast(new Object[size]);
+  @Override
+  public boolean containsAll(Collection<?> c) {
+    boolean rv = true;
+    Iterator it = c.iterator();
+    while (rv && it.hasNext())
+      rv = contains(it.next());
+    return rv;
+  }
+
+  @Override
+  public boolean addAll(Collection<? extends E> c) {
+    for (E e : c)
+      this.add(e);
+    return true;
+  }
+
+  @Override
+  public boolean removeAll(Collection<?> c) {
+    E tdata[] = Utils.cast(new Object[this.data.length]);
+    int tsize = 0;
+    for (E e : this)
+      if (!c.contains(e))
+        tdata[tsize++] = e;
+
+    boolean rv = tsize != size;
+    this.data = null;
+    this.data = tdata;
+    this.size = tsize;
+    this.head = 0;
+    return rv;
+  }
+
+  @Override
+  public boolean retainAll(Collection<?> c) {
+    E tdata[] = Utils.cast(new Object[this.data.length]);
+    int tsize = 0;
+    for (E e : this)
+      if (c.contains(e))
+        tdata[tsize++] = e;
+    boolean rv = tsize != size;
+    this.data = null;
+    this.data = tdata;
+    this.size = tsize;
+    this.head = 0;
+    return rv;
+  }
+
+  @Override
+  public void clear() {
+    this.head = 0;
+    this.size = 0;
+  }
+
+  @Override
+  public boolean offer(E e) {
+    add(e);
+    return true;
+  }
+
+  @Override
+  public E remove() {
+    if (isEmpty())
+      throw new NoSuchElementException();
+    E rv = data[head];
+    head = (head + 1) % data.length;
+    size--;
+    return rv;
+  }
+
+  @Override
+  public E poll() {
+    E rv = null;
+    if (!isEmpty()) {
+      rv = data[head];
+      head = (head + 1) % data.length;
+      size--;
+    }
+    return rv;
+  }
+
+  @Override
+  public E element() {
+    if (isEmpty())
+      throw new NoSuchElementException();
+    return data[head];
+  }
+
+  @Override
+  public E peek() {
+    E rv = null;
+    if (!isEmpty())
+      rv = data[head];
+    return rv;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  /**
+   * Returns true if this collection is full.
+   *
+   * @return true if this collection is full
+   */
+  public boolean isFull() {
+    return size == data.length;
+  }
+
+  /**
+   * Returns the middle element in the queue.
+   * It is usefull to gather neighborhoods of elements.
+   *
+   * @return the middle element in the queue
+   * @throws NoSuchElementException if this queue is empty
+   */
+  public E middle() {
+    if (isEmpty())
+      throw new NoSuchElementException();
+    return data[(head + (size / 2)) % data.length];
+  }
+
+  /**
+   * Returns the middle elements in the queue.
+   * It is usefull to gather neighborhoods of elements.
+   * Copies the references of the middle elements to the array.
+   *
+   * @param array where the references from the middle objects are stored
+   * @throws NoSuchElementException   if this queue is empty
+   * @throws IllegalArgumentException if the parity of the array and the queue are not the same
+   */
+  public void middle(E array[]) {
+    if (isEmpty())
+      throw new NoSuchElementException();
+    if (size > array.length && array.length % 2 != size % 2)
+      throw new IllegalArgumentException();
+    int idx = ((head + (size / 2)) % data.length) - (array.length / 2);
+    for (int i = 0; i < array.length; i++, idx++)
+      array[i] = data[idx];
+  }
+
+  /**
+   * Returns the maximum number of elements in the queue.
+   *
+   * @return the maximum number of elements in the queue
+   */
+  public int length() {
+    return data.length;
+  }
+
+  @Override
+  public boolean contains(Object o) {
+    boolean rv = false;
+    Iterator it = iterator();
+    while (rv && it.hasNext())
+      rv = it.next().equals(o);
+    return rv;
+  }
+
+  @Override
+  public int size() {
+    return size;
+  }
+
+  @Override
+  public Iterator<E> iterator() {
+    return new CircularQueueIterator();
+  }
+
+
+  @Override
+  public <T> T[] toArray(T[] a) {
+    int size = 0;
+    for (E e : this)
+      a[size++] = Utils.cast(e);
+    return a;
+  }
+
+  @Override
+  public Object[] toArray() {
+    E rv[] = Utils.cast(new Object[data.length]);
+    int i = 0;
+    for (E e : this)
+      rv[i++] = e;
+    return rv;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("[");
+    Iterator it = iterator();
+    while (it.hasNext()) {
+      sb.append(it.next());
+      if (it.hasNext())
+        sb.append(", ");
+    }
+    sb.append(']');
+    return sb.toString();
+  }
+
+  /**
+   * CircularQueue iterator
+   */
+  private class CircularQueueIterator implements Iterator<E> {
+    private int i = head, count = size;
+
+    @Override
+    public boolean hasNext() {
+      return count > 0;
     }
 
     @Override
-    public boolean add(E e) {
-        if (e == null)
-            throw new NullPointerException();
-
-        int tail = (head + size) % data.length;
-        data[tail] = e;
-        if (size == data.length)
-            head = (head + 1) % data.length;
-        else
-            size++;
-
-        return true;
+    public E next() {
+      E rv = data[i];
+      i = (i + 1) % data.length;
+      count--;
+      return rv;
     }
-
-    @Override
-    public boolean remove(Object o) {
-        boolean rv = false;
-        if (contains(o)) {
-            E tdata[] = Utils.cast(new Object[this.data.length]);
-            int tsize = 0;
-            for (E e : this)
-                if (!e.equals(o))
-                    tdata[tsize++] = e;
-            this.data = null;
-            this.data = tdata;
-            this.size = tsize;
-            this.head = 0;
-            rv = true;
-        }
-        return rv;
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        boolean rv = true;
-        Iterator it = c.iterator();
-        while (rv && it.hasNext())
-            rv = contains(it.next());
-        return rv;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends E> c) {
-        for (E e : c)
-            this.add(e);
-        return true;
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        E tdata[] = Utils.cast(new Object[this.data.length]);
-        int tsize = 0;
-        for (E e : this)
-            if (!c.contains(e))
-                tdata[tsize++] = e;
-
-        boolean rv = tsize != size;
-        this.data = null;
-        this.data = tdata;
-        this.size = tsize;
-        this.head = 0;
-        return rv;
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        E tdata[] = Utils.cast(new Object[this.data.length]);
-        int tsize = 0;
-        for (E e : this)
-            if (c.contains(e))
-                tdata[tsize++] = e;
-        boolean rv = tsize != size;
-        this.data = null;
-        this.data = tdata;
-        this.size = tsize;
-        this.head = 0;
-        return rv;
-    }
-
-    @Override
-    public void clear() {
-        this.head = 0;
-        this.size = 0;
-    }
-
-    @Override
-    public boolean offer(E e) {
-        add(e);
-        return true;
-    }
-
-    @Override
-    public E remove() {
-        if (isEmpty())
-            throw new NoSuchElementException();
-        E rv = data[head];
-        head = (head + 1) % data.length;
-        size--;
-        return rv;
-    }
-
-    @Override
-    public E poll() {
-        E rv = null;
-        if (!isEmpty()) {
-            rv = data[head];
-            head = (head + 1) % data.length;
-            size--;
-        }
-        return rv;
-    }
-
-    @Override
-    public E element() {
-        if (isEmpty())
-            throw new NoSuchElementException();
-        return data[head];
-    }
-
-    @Override
-    public E peek() {
-        E rv = null;
-        if (!isEmpty())
-            rv = data[head];
-        return rv;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    /**
-     * Returns true if this collection is full.
-     *
-     * @return true if this collection is full
-     */
-    public boolean isFull() {
-        return size == data.length;
-    }
-
-    /**
-     * Returns the middle element in the queue.
-     * It is usefull to gather neighborhoods of elements.
-     *
-     * @return the middle element in the queue
-     * @throws NoSuchElementException if this queue is empty
-     */
-    public E middle() {
-        if (isEmpty())
-            throw new NoSuchElementException();
-        return data[(head + (size / 2)) % data.length];
-    }
-
-    /**
-     * Returns the middle elements in the queue.
-     * It is usefull to gather neighborhoods of elements.
-     * Copies the references of the middle elements to the array.
-     *
-     * @param array where the references from the middle objects are stored
-     * @throws NoSuchElementException   if this queue is empty
-     * @throws IllegalArgumentException if the parity of the array and the queue are not the same
-     */
-    public void middle(E array[]) {
-        if (isEmpty())
-            throw new NoSuchElementException();
-        if (size > array.length && array.length % 2 != size % 2)
-            throw new IllegalArgumentException();
-        int idx = ((head + (size / 2)) % data.length) - (array.length / 2);
-        for (int i = 0; i < array.length; i++, idx++)
-            array[i] = data[idx];
-    }
-
-    /**
-     * Returns the maximum number of elements in the queue.
-     *
-     * @return the maximum number of elements in the queue
-     */
-    public int length() {
-        return data.length;
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        boolean rv = false;
-        Iterator it = iterator();
-        while (rv && it.hasNext())
-            rv = it.next().equals(o);
-        return rv;
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public Iterator<E> iterator() {
-        return new CircularQueueIterator();
-    }
-
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        int size = 0;
-        for (E e : this)
-            a[size++] = Utils.cast(e);
-        return a;
-    }
-
-    @Override
-    public Object[] toArray() {
-        E rv[] = Utils.cast(new Object[data.length]);
-        int i = 0;
-        for (E e : this)
-            rv[i++] = e;
-        return rv;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("[");
-        Iterator it = iterator();
-        while (it.hasNext()) {
-            sb.append(it.next());
-            if (it.hasNext())
-                sb.append(", ");
-        }
-        sb.append(']');
-        return sb.toString();
-    }
-
-    /**
-     * CircularQueue iterator
-     */
-    private class CircularQueueIterator implements Iterator<E> {
-        private int i = head, count = size;
-
-        @Override
-        public boolean hasNext() {
-            return count > 0;
-        }
-
-        @Override
-        public E next() {
-            E rv = data[i];
-            i = (i + 1) % data.length;
-            count--;
-            return rv;
-        }
-    }
+  }
 }
--- src/test/java/pt/it/av/atnog/utils/structures/CircularQueueTest.java
+++ src/test/java/pt/it/av/atnog/utils/structures/CircularQueueTest.java
@@ -3,6 +3,7 @@ package pt.it.av.atnog.utils.structures;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 import static org.junit.Assert.assertTrue;
@@ -14,80 +15,110 @@ import static org.junit.Assert.assertTrue;
  * @version 1.0
  */
 public class CircularQueueTest {
+  private static String data3[] = {"A", "B", "C"};
+  private static String data5[] = {"A", "B", "C", "D", "E"};
 
-    @Test(expected = NoSuchElementException.class)
-    public void test_middle_empty() {
-        CircularQueue<String> q = new CircularQueue(3);
-        q.middle();
+  @Test
+  public void test_insert_full() {
+    CircularQueue<String> queue = new CircularQueue(3);
+    for (String datum : data3) {
+      queue.add(datum);
     }
 
-    @Test(expected = NoSuchElementException.class)
-    public void test_middle_array_empty() {
-        CircularQueue<String> q = new CircularQueue(3);
-        String array[] = new String[1];
-        q.middle(array);
+    Iterator<String> it = queue.iterator();
+    int i = 0;
+    while (it.hasNext()) {
+      assertTrue(it.next().equals(data3[i++]));
+    }
+    assertTrue(i == queue.size());
+  }
+
+  @Test
+  public void test_insert_overfull() {
+    CircularQueue<String> queue = new CircularQueue(3);
+    for (String datum : data5) {
+      queue.add(datum);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void test_middle_array_parity() {
-        CircularQueue<String> q = new CircularQueue(3);
-        q.add("A");
-        q.add("B");
-        q.add("C");
-        String array[] = new String[2];
-        q.middle(array);
+    Iterator<String> it = queue.iterator();
+    int i = 0;
+    while (it.hasNext()) {
+      assertTrue(it.next().equals(data5[i++ + (data5.length - queue.size())]));
     }
+    assertTrue(i == queue.size());
+  }
 
-    @Test
-    public void test_middle_odd() {
-        CircularQueue<String> q = new CircularQueue(3);
-        q.add("A");
-        assertTrue(q.middle().equals("A"));
-        q.add("B");
-        q.add("C");
-        assertTrue(q.middle().equals("B"));
-    }
+  @Test(expected = NoSuchElementException.class)
+  public void test_middle_empty() {
+    CircularQueue<String> q = new CircularQueue(3);
+    q.middle();
+  }
 
-    @Test
-    public void test_middle_even() {
-        CircularQueue<String> q = new CircularQueue(3);
-        q.add("A");
-        q.add("B");
-        assertTrue(q.middle().equals("B"));
-    }
+  @Test(expected = NoSuchElementException.class)
+  public void test_middle_array_empty() {
+    CircularQueue<String> q = new CircularQueue(3);
+    String array[] = new String[1];
+    q.middle(array);
+  }
 
-    @Test
-    public void test_middle_array_odd() {
-        CircularQueue<String> q = new CircularQueue(5);
-        q.add("A");
-        String array[] = new String[1];
-        q.middle(array);
-        String r1[] = {"A"};
-        assertTrue(Arrays.equals(array, r1));
-        q.add("B");
-        q.add("C");
-        array = new String[3];
-        q.middle(array);
-        String r2[] = {"A", "B", "C"};
-        assertTrue(Arrays.equals(array, r2));
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void test_middle_array_parity() {
+    CircularQueue<String> q = new CircularQueue(3);
+    q.add("A");
+    q.add("B");
+    q.add("C");
+    String array[] = new String[2];
+    q.middle(array);
+  }
 
-    @Test
-    public void test_middle_array_even() {
-        CircularQueue<String> q = new CircularQueue(5);
-        q.add("A");
-        q.add("B");
-        String array[] = new String[2];
-        q.middle(array);
-        String r1[] = {"A", "B"};
-        assertTrue(Arrays.equals(array, r1));
-        q.add("C");
-        q.add("D");
-        array = new String[4];
-        q.middle(array);
-        String r2[] = {"A", "B", "C", "D"};
-        assertTrue(Arrays.equals(array, r2));
-    }
+  @Test
+  public void test_middle_odd() {
+    CircularQueue<String> q = new CircularQueue(3);
+    q.add("A");
+    assertTrue(q.middle().equals("A"));
+    q.add("B");
+    q.add("C");
+    assertTrue(q.middle().equals("B"));
+  }
 
+  @Test
+  public void test_middle_even() {
+    CircularQueue<String> q = new CircularQueue(3);
+    q.add("A");
+    q.add("B");
+    assertTrue(q.middle().equals("B"));
+  }
 
+  @Test
+  public void test_middle_array_odd() {
+    CircularQueue<String> q = new CircularQueue(5);
+    q.add("A");
+    String array[] = new String[1];
+    q.middle(array);
+    String r1[] = {"A"};
+    assertTrue(Arrays.equals(array, r1));
+    q.add("B");
+    q.add("C");
+    array = new String[3];
+    q.middle(array);
+    String r2[] = {"A", "B", "C"};
+    assertTrue(Arrays.equals(array, r2));
+  }
+
+  @Test
+  public void test_middle_array_even() {
+    CircularQueue<String> q = new CircularQueue(5);
+    q.add("A");
+    q.add("B");
+    String array[] = new String[2];
+    q.middle(array);
+    String r1[] = {"A", "B"};
+    assertTrue(Arrays.equals(array, r1));
+    q.add("C");
+    q.add("D");
+    array = new String[4];
+    q.middle(array);
+    String r2[] = {"A", "B", "C", "D"};
+    assertTrue(Arrays.equals(array, r2));
+  }
 }
